036fb9504a08cdfcbe4a29ef39e962b965f85412
Sébastien Weber <seba.weber@gmail.com>
2021-03-13 00:34:56 +0100

    Update pnps.py
    added a patch on the sellmeir range issue (from N Geib)

--- src/pymodaq_femto/pnps.py
+++ src/pymodaq_femto/pnps.py
@@ -1,30 +1,44 @@
 from pypret.pnps import CollinearPNPS
+from pypret.frequencies import wl2om
 import numpy as np
 
 
 
+
 class DSCAN(CollinearPNPS):
-    """ Implements the dispersion scan method [Miranda2012a]_ [Miranda2012b]_.
+    """ Implements the dispersion scan method with Fresnel reflection.
+
+    Subclassing d-scan to support fresnel reflection.
     """
-    method = "dscan"
+
     parameter_name = "insertion"
     parameter_unit = "m"
+    method = "dscan"
 
-    def __init__(self, pulse, process, material):
-        """ Creates the instance.
+    def __init__(self, pulse, process, material, **kwargs):
+        super().__init__(
+            pulse, process, material=material, **kwargs
+        )
 
-        Parameters
-        ----------
-        pulse : Pulse instance
-            The pulse object that defines the simulation grid.
-        process : str
-            The nonlinear process used in the PNPS method.
-        material : str
-            The wedge material.
-        """
-        super().__init__(pulse, process, material=material)
+    def _post_init(self):
+        super()._post_init()
+        w = self.ft.w + self.w0
+        # use only the valid range of the Sellmeier equations
+        w1, w2 = sorted(wl2om(np.array(self.material._range)))
+        valid = (w >= w1) & (w <= w2)
+        w = w[valid]
+        # remove first and second Taylor order
+        k = self.material.k(w, unit="om")
+        k0 = self.material.k(self.w0, unit="om")
+        k1 = self.material.k(self.w0 + self.ft.dw, unit="om")
+        dk = (k1 - k0) / self.ft.dw
+        self._k = k - k0 - dk * self.ft.w[valid]
+        self._n = self.material.n(w, unit="om")
+        self._mask_valid = valid
 
     def mask(self, insertion):
-        w = self.ft.w + self.w0
-        k = self.material.k(w, unit="om")
-        return np.exp(1.0j * k * insertion)
+        if insertion == 0.0:
+            return np.ones(self.ft.N, dtype=np.complex128)
+        H = np.zeros(self.ft.N, dtype=np.complex128)
+        H[self._mask_valid] = np.exp(1.0j * self._k * insertion)
+        return H

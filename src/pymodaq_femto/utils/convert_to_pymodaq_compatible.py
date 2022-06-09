057f20a8b7d4384f7ea0bbfa1335e09434f432a3
Sébastien Weber <seba.weber@gmail.com>
2022-06-09 10:20:17 +0200

    Update convert_to_pymodaq_compatible.py

--- src/pymodaq_femto/utils/convert_to_pymodaq_compatible.py
+++ src/pymodaq_femto/utils/convert_to_pymodaq_compatible.py
@@ -33,20 +33,31 @@ class DScanCustomSaver(H5SaverBase):
 
 if __name__ == '__main__':
     from pathlib import Path
+    saver = DScanCustomSaver()
 
+    # # One particular implementation of saved data
     path_root = Path(__file__).parent.parent.parent.parent
     pathToLoad = path_root.joinpath("data/dscan_bank/")
     pathToSave = path_root.joinpath("data/dscan_bank_pymodaq/")
     if not pathToSave.is_dir():
         pathToSave.mkdir()
-
-    saver = DScanCustomSaver()
-
     fileIndex = 6
     # Load data
     fileName = "measured_dscan_" + str(fileIndex) + ".h5"
     pulse_from_spectrum, retrieved_pulse, measured_dscan, retrieved_dscan, raw_spectrum, folder = pypret.load(
         str(pathToLoad.joinpath(fileName)))
+    insertion = measured_dscan.axes[0]
+    spectrum_trace_axis = measured_dscan.axes[1]
+    spectrum_fundamental_intensity = raw_spectrum.intensity
+    spectrum_fundamental_axis_wavelength = raw_spectrum.wl
+    trace_data = measured_dscan.data
+
+    # # template to use:
+    # trace_data = my_experimental_data_trace_as_2D_numpy_array
+    # insertion = my_experimental_insertion_as_numpy_array
+    # spectrum_trace_axis = my_experimental_trace_wavelength_axis_as_numpy_array
+    # spectrum_fundamental_intensity = my_experimental_fundamental_spectrum_as_numpy_array
+    # spectrum_fundamental_axis_wavelength = my_experimental_fundamental_spectrum_wavelength_axis_as_numpy_array
 
     # Open file and create scan node
     saver.init_file(addhoc_file_path=str(pathToSave.joinpath(fileName)), update_h5=True)
@@ -54,9 +65,9 @@ if __name__ == '__main__':
     scannode.set_attr('scan_type', "Scan1D")
 
     # Add all data
-    saver.add_exp_insertion(scannode, measured_dscan.axes[0])
-    saver.add_exp_trace(scannode, measured_dscan.data, measured_dscan.axes[1])
-    saver.add_exp_fundamental(scannode, raw_spectrum.intensity, raw_spectrum.wl)
+    saver.add_exp_insertion(scannode, insertion)
+    saver.add_exp_trace(scannode, trace_data, spectrum_trace_axis)
+    saver.add_exp_fundamental(scannode, raw_spectrum.intensity, spectrum_fundamental_axis_wavelength)
     saver.close_file()
     pass
 
